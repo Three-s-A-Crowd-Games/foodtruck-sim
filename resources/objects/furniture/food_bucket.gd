b646fa43a0070173ffc1df3b4e7a47cb3c3a6972
Jakl <57708725+Jakllp@users.noreply.github.com>
2024-04-28 13:44:06 +0200

    Make bunboxes functional

--- resources/objects/furniture/food_bucket.gd
+++ resources/objects/furniture/food_bucket.gd
@@ -2,15 +2,6 @@ extends Node3D
 
 @export var item_scene:PackedScene = preload("res://resources/cube.tscn")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-	
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-	
 
 func _on_exit_detector_body_exited(body):
 	body = body as Food
