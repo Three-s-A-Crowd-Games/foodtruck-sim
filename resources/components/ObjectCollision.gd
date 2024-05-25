aa2acf5754afc8093ef56981130e5e2aeea4bbf9
Jakl <57708725+Jakllp@users.noreply.github.com>
2024-05-25 21:56:03 +0200

    Prettified collision-shapes

---
+++ resources/components/ObjectCollision.gd
@@ -0,0 +1,18 @@
+extends XRToolsCollisionHand
+
+var shape_node :CollisionShape3D
+
+func _on_function_pickup_has_picked_up(what):
+	if (not what is XRToolsInteractableHandle):
+		var orig_node :Node3D = what.get_node("CollisionShape3D")
+		shape_node = orig_node.duplicate()
+		add_child(shape_node)
+		shape_node.rotation = orig_node.global_rotation
+		shape_node.global_position = orig_node.global_position
+		collision_mask = 1
+
+
+func _on_function_pickup_has_dropped():
+	remove_child(shape_node)
+	shape_node = null
+	collision_mask = 0
