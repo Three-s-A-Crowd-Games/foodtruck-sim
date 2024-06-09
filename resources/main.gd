711f49ef33388a77b331398b2748e516eda69000
Jakl <57708725+Jakllp@users.noreply.github.com>
2024-06-09 21:02:17 +0200

    Trailer Final

--- resources/main.gd
+++ resources/main.gd
@@ -16,12 +16,12 @@ func _ready():
 	else:
 		print("OpenXR not initialized, please check if your headset is connected")
 	
-	#var xr_orig = null
-	#if xr_interface.is_hand_tracking_supported():
-	#	XRToolsFunctionPickup.in_handtracking_mode = true
-	#else:
-	#	xr_orig = load("res://resources/xr_origin_controller.tscn").instantiate()
-	var xr_orig = load("res://resources/xr_origin_handtracking.tscn").instantiate()
+	var xr_orig = null
+	if xr_interface.is_hand_tracking_supported():
+		XRToolsFunctionPickup.in_handtracking_mode = true
+		xr_orig = load("res://resources/xr_origin_handtracking.tscn").instantiate()
+	else:
+		xr_orig = load("res://resources/xr_origin_controller.tscn").instantiate()
 	xr_orig.transform = Transform3D(Vector3(1.31134e-07, 0, 1), Vector3(0, 1, 0), Vector3(-1, 0, 1.31134e-07), Vector3(-0.109164, 1.43315, 0.152627))
 	add_child(xr_orig)
 	
