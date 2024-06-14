d159079dfb3372711d28ebf20efa6cb407d8a2a9
Thyrexx <96577754+Thyrexx@users.noreply.github.com>
2024-06-14 19:38:05 +0200

    updated test helper scenes

--- resources/test_helpers/math_tests.gd
+++ resources/test_helpers/math_tests.gd
@@ -4,9 +4,13 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var up = Vector3.UP
-	var l = Vector3(-1,1,0)
-	var r = Vector3(1,1,0)
+	var node = Node3D.new()
+	#var basis = Basis.FLIP_Y
+	#node.transform.basis = basis
+	node.rotate_z(PI)
+	node.rotate_y(PI)
+	
+	add_child(node)
 	
 	printt(Vector3(0.25,0,0).rotated(Vector3.FORWARD, PI/2))
 
