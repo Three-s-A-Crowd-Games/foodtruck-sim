6ad8c2918c60753810bc09a6f047688d983f0c1f
Jakllp <57708725+Jakllp@users.noreply.github.com>
2024-06-16 00:16:05 +0200

    fixed must_haves , NO_DOUBLE_STACK constraint

--- resources/base_types/ingredient.gd
+++ resources/base_types/ingredient.gd
@@ -27,54 +27,64 @@ enum Type{
 
 enum Constraints{
 	POSITION,
-	MAX_USE
+	MAX_USE,
+	NO_DOUBLE_STACK
 }
 
 static var ingredients: Dictionary = {
 	Type.BUN_TOP : {
-		Category : [Category.BURGER_PART],
+		Category : Category.BURGER_PART,
 		Constraints.POSITION : -1,
 		Constraints.MAX_USE : 1
 		},
 	Type.BUN_BOTTOM : {
-		Category : [Category.BURGER_PART],
+		Category : Category.BURGER_PART,
 		Constraints.POSITION : 0,
 		Constraints.MAX_USE : 1
 		},
 	Type.PATTY : {
-		Category : [Category.BURGER_PART]
+		Category : Category.BURGER_PART
 		},
 	Type.CHEESE : {
-		Category : [Category.BURGER_PART]
+		Category : Category.BURGER_PART
 		},
 	Type.LETTUCE : {
-		Category : [Category.BURGER_PART]
+		Category : Category.BURGER_PART
 		},
 	Type.ONION : {
-		Category : [Category.BURGER_PART]
+		Category : Category.BURGER_PART
 		},
 	Type.CUCUMBER : {
-		Category : [Category.BURGER_PART]
+		Category : Category.BURGER_PART
 		},
 	Type.TOMATO : {
-		Category : [Category.BURGER_PART]
+		Category : Category.BURGER_PART
 		},
 	Type.FRIES : {
-		Category : [Category.FRIES]
+		Category : Category.FRIES
 		},
 	Type.WATER: {
-		Category : [Category.DRINKS]
+		Category : Category.DRINKS
 	},
 	Type.KETCHUP: {
-		Category : [Category.SAUCES]
+		Category : Category.SAUCES
 	},
 }
 
 static var categories: Dictionary = {
-	Category.BURGER_PART : [Type.BUN_TOP, Type.BUN_BOTTOM, Type.PATTY, Type.ONION, Type.CHEESE, Type.CUCUMBER, Type.TOMATO, Type.LETTUCE],
-	Category.FRIES : [Type.FRIES],
-	Category.DRINKS : [Type.WATER],
-	Category.SAUCES : [Type.KETCHUP]
+	Category.BURGER_PART : {
+		Type : [Type.BUN_TOP, Type.BUN_BOTTOM, Type.PATTY, Type.ONION, Type.CHEESE, Type.CUCUMBER, Type.TOMATO, Type.LETTUCE]
+	},
+	Category.FRIES : {
+		Type : [Type.FRIES]
+	},
+	Category.DRINKS : {
+		Type : [Type.WATER]
+	},
+	Category.SAUCES : {
+		Type : [Type.KETCHUP],
+		Constraints.NO_DOUBLE_STACK : true
+	}
 }
 
 static func parse_ingridient_list(le_array :Array) -> String: #Alda was is Godoooo für ne Language.
--- resources/base_types/recipe.gd
+++ resources/base_types/recipe.gd
@@ -57,7 +57,7 @@ static func create_recipe(type: Type) -> Recipe:
 	
 	# gather all the ingredients that can be used for this type of recipe
 	for ingredient_category in recipes[type][Ingredient.Category]:
-		possible_ingredients += Ingredient.categories[ingredient_category]
+		possible_ingredients += Ingredient.categories[ingredient_category][Ingredient.Type]
 	
 	# Check if this recipe type has the MINIMUM_AMOUNT constraint
 	if recipes[type].has(Constraints.MINIMUM_AMOUNT):
@@ -70,6 +70,9 @@ static func create_recipe(type: Type) -> Recipe:
 	
 	# take a random amount of ingredients
 	var amount = randi_range(min_amount, max_amount)
+	var compl_amount = amount
+	#free_moving_amount is important for double stacking -> tells us how many movable objects there are
+	var free_moving_amount = amount
 	new_recipe.ingredients.resize(amount)
 	new_recipe.ingredients.fill(-1)
 	
@@ -77,6 +80,7 @@ static func create_recipe(type: Type) -> Recipe:
 	# fill the ingredients array of the new recipe with random ingredients from the possible ingredients
 	# take random elements from the pool of possible ingredients
 	var used_ingredients: Array[Ingredient.Type]
+	var open_positions = range(amount)
 	if(recipes[type].has(Constraints.MUST_HAVE)):
 		for must_have in recipes[type][Constraints.MUST_HAVE]:
 			var ingr
@@ -87,15 +91,27 @@ static func create_recipe(type: Type) -> Recipe:
 			used_ingredients.append(ingr)
 			if(Ingredient.ingredients[ingr].has(Ingredient.Constraints.MAX_USE) and used_ingredients.count(ingr) >= Ingredient.ingredients[ingr][Ingredient.Constraints.MAX_USE]):
 				possible_ingredients.erase(ingr)
+				#Lets see if it is immobile
+				if(Ingredient.ingredients[ingr].has(Ingredient.Constraints.POSITION)):
+					free_moving_amount -= 1
+			amount -= 1
 	
+	var no_double_stack_amount = free_moving_amount/2
+	if free_moving_amount%2 != 0:
+		no_double_stack_amount += 1
 	for i in range(amount):
 		var ingr = possible_ingredients.pick_random()
 		used_ingredients.append(ingr)
+		# Handle Max_Amount
 		if(Ingredient.ingredients[ingr].has(Ingredient.Constraints.MAX_USE) and used_ingredients.count(ingr) >= Ingredient.ingredients[ingr][Ingredient.Constraints.MAX_USE]):
 			possible_ingredients.erase(ingr)
+		# Handle No_Double_Stack (make sure there's not more of the category in there then we are able to sort without double stacking)
+		if(Ingredient.categories[Ingredient.ingredients[ingr][Ingredient.Category]].has(Ingredient.Constraints.NO_DOUBLE_STACK) and get_amount_of_category(used_ingredients, Ingredient.ingredients[ingr][Ingredient.Category]) >= no_double_stack_amount):
+			# Remove all ingredients of that category
+			for pos_ingr in possible_ingredients:
+				if Ingredient.ingredients[pos_ingr][Ingredient.Category] == Ingredient.ingredients[ingr][Ingredient.Category]:
+					possible_ingredients.erase(pos_ingr)
 	
-	
-	var open_positions = range(amount)
 	var used_ingredients_copy = used_ingredients.duplicate()
 	for ingr in used_ingredients_copy:
 		# check if this ingredient has a position constraint
@@ -108,11 +124,48 @@ static func create_recipe(type: Type) -> Recipe:
 			if position >= 0: 
 				open_positions.erase(position)
 			else:
-				open_positions.erase(amount + position)
+				open_positions.erase(compl_amount + position)
 	
-	for pos in open_positions:
-			assert(used_ingredients.size() > 0, "no more open positions")
-			new_recipe.ingredients[pos] = used_ingredients.pop_back()
+	#Semi-Randomly place all other ingredients
+	var all_placed = false
+	while(!all_placed):
+		all_placed = true
+		var placed_ingredients = new_recipe.ingredients.duplicate()
+		var open_positions_dub = open_positions.duplicate()
+		for ingr in used_ingredients:
+			var pos = open_positions_dub.pick_random()
+			
+			var ingr_cat = Ingredient.ingredients[ingr][Ingredient.Category]
+			#Lets make sure that we value NO_DOUBLE_STACKS
+			if(Ingredient.categories[ingr_cat].has(Ingredient.Constraints.NO_DOUBLE_STACK)):
+				# Check if infront and behind are ok:
+				# Either first/last one OR it's empty OR it's not of the same category
+				var infront_okay = pos == 0 or placed_ingredients[pos-1] == -1 or ingr_cat != Ingredient.ingredients[placed_ingredients[pos-1]][Ingredient.Category]
+				var behind_okay = pos == compl_amount - 1 or placed_ingredients[pos+1] == -1 or ingr_cat != Ingredient.ingredients[placed_ingredients[pos+1]][Ingredient.Category]
+				# If this pos is not okay check if any open position is okay
+				if(!infront_okay or !behind_okay):
+					all_placed = false
+					for inner_check_pos in open_positions_dub:
+						infront_okay = inner_check_pos == 0 or ingr_cat != Ingredient.ingredients[placed_ingredients[inner_check_pos-1]][Ingredient.Category]
+						behind_okay = inner_check_pos == compl_amount - 1 or ingr_cat != Ingredient.ingredients[placed_ingredients[inner_check_pos+1]][Ingredient.Category]
+						if infront_okay and behind_okay:
+							# Found good Pos
+							all_placed = true
+							pos = inner_check_pos
+							break
+				if !all_placed:
+					# Try again
+					break
+			
+			placed_ingredients[pos] = ingr
+			open_positions_dub.erase(pos)
+		new_recipe.ingredients = placed_ingredients
 	
 	return new_recipe
 	
+static func get_amount_of_category(ingr_list :Array, cat :Ingredient.Category) -> int:
+	var count = 0
+	for ingr in ingr_list:
+		if Ingredient.ingredients[ingr][Ingredient.Category] == cat:
+			count += 1
+	return count
