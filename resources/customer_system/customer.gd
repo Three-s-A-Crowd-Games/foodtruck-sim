2c03d33f04c7fe509f4ca1bdb5335cb47fb8d133
Jakl <57708725+Jakllp@users.noreply.github.com>
2024-06-21 23:56:53 +0200

    Customer randomization

--- resources/customer_system/customer.gd
+++ resources/customer_system/customer.gd
@@ -1,13 +1,36 @@
 class_name Customer
 extends Node3D
 
+const Colors := [
+	Color("00B3E7"), #Standard Blue
+	Color("0B00FF"), #Deep Blue
+	Color("5300A4"), #Lila
+	Color("FF3E00"), #Orange
+	Color("00FFA1"), #Türkis
+	Color("FF00EB"), #Pink
+	Color("00E700"), #Green
+	Color("FFFFFF"), #White
+	]
+
+const Accessoires := [
+	null,
+	preload("res://assets/models/accessoires/schnelle_brille.blend")
+]
+
 var wait_pos :Path3D
 var can_leave := false
 var waiting_pos_stop_value = 0.0
 var tray :Tray = null
 
 func randomize_appearance():
-	pass
+	var main_mat := StandardMaterial3D.new()
+	main_mat.albedo_color = Colors.pick_random()
+	$customer/Cone.set_surface_override_material(0, main_mat)
+	
+	var acc_raw = Accessoires.pick_random()
+	var acc = null
+	if acc_raw != null: acc = acc_raw.instantiate()
+	if acc != null: $customer.add_child(acc)
 
 func angry():
 	can_leave = true
