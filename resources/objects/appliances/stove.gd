ada0159ebc6e7af848cd6ae65e1844cfc95b1994
Thyrexx <96577754+Thyrexx@users.noreply.github.com>
2024-06-21 09:16:26 +0200

    FS-135 stove knob sounds

--- resources/objects/appliances/stove.gd
+++ resources/objects/appliances/stove.gd
@@ -2,10 +2,18 @@ extends Node3D
 
 @onready var knob_model: MeshInstance3D  = $"stove-v2/knob"
 @onready var cooking_area: CookingArea = $CookingArea
+@onready var audio_player: AudioStreamPlayer3D = $AudioStreamPlayer3D
+
+var sound_on: AudioStream = preload("res://audio/object_interaction/knob/knob-on.mp3")
+var sound_off: AudioStream = preload("res://audio/object_interaction/knob/knob-off.mp3")
 
 func _on_toggle_stove(stove_on :bool):
 	if(stove_on):
 		knob_model.rotation_degrees.z = 90
+		audio_player.stream = sound_on
+		audio_player.play()
 	else:
 		knob_model.rotation_degrees.z = 0
+		audio_player.stream = sound_off
+		audio_player.play()
 	cooking_area.enabled = !cooking_area.enabled
