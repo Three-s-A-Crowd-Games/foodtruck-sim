83ad8b4a4806849e547f7acc5aeb54d903f2e194
Thyrexx <96577754+Thyrexx@users.noreply.github.com>
2024-06-21 10:52:12 +0200

    FS-135 fridge interaction

---
+++ resources/objects/appliances/fridge.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+@export var is_open := false
+@onready var audio_player := $AudioStreamPlayer3D
+@export var sound_open: AudioStream = preload("res://audio/object_interaction/fridge/fridge-open.mp3")
+@export var sound_close: AudioStream = preload("res://audio/object_interaction/fridge/fridge-close.mp3")
+
+func _on_xr_tools_interactable_hinge_hinge_moved(angle: Variant) -> void:
+	if angle == 0:
+		audio_player.stream = sound_close
+		audio_player.play()
+		is_open = false
+	elif not is_open:
+		audio_player.stream = sound_open
+		audio_player.play()
+		is_open = true
--- resources/objects/appliances/stove.gd
+++ resources/objects/appliances/stove.gd
@@ -1,11 +1,11 @@
 extends Node3D
 
+@export var sound_on: AudioStream = preload("res://audio/object_interaction/knob/knob-on.mp3")
+@export var sound_off: AudioStream = preload("res://audio/object_interaction/knob/knob-off.mp3")
+
 @onready var knob_model: MeshInstance3D  = $"stove-v2/knob"
 @onready var cooking_area: CookingArea = $CookingArea
 @onready var audio_player: AudioStreamPlayer3D = $AudioStreamPlayer3D
-
-var sound_on: AudioStream = preload("res://audio/object_interaction/knob/knob-on.mp3")
-var sound_off: AudioStream = preload("res://audio/object_interaction/knob/knob-off.mp3")
 
 func _on_toggle_stove(stove_on :bool):
 	if(stove_on):
