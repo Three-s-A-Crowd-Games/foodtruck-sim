8d7145deee5e10f4d9601eae628888954289755e
Thyrexx <96577754+Thyrexx@users.noreply.github.com>
2024-06-25 15:53:00 +0200

    Fix stack reverse bug with cheese

--- resources/base_types/burger_part.gd
+++ resources/base_types/burger_part.gd
@@ -30,14 +30,14 @@ func _ready():
 	assert(mesh_nodes.size() == 1, "This Burger Part has either 0 or more than one mesh nodes, which can't be handled so far.")
 	var mesh_node: MeshInstance3D = mesh_nodes[0]
 	var aabb = mesh_node.mesh.get_aabb()
-	height = aabb.size.y
+	height = aabb.size.y + aabb.position.y
 	if mesh_node.get_parent() == self:
 		stack_zone_distance = height + mesh_node.position.y
 	else:
 		stack_zone_distance = height + mesh_node.get_parent().position.y
 	
 	original_center_of_mass.y = stack_zone_distance - height/2
-	stack_zone_distance += aabb.position.y
+	#stack_zone_distance += aabb.position.y
 	stack_zone_distance += burger_part_seperation_distance
 	stack_zone.position.y = stack_zone_distance
 	
