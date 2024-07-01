9e900eabcd3b3384d92b4c4e493c85d25ed497d9
Thyrexx <96577754+Thyrexx@users.noreply.github.com>
2024-07-01 19:33:33 +0200

    FS-135 order button sounds

--- resources/objects/next_order_button.gd
+++ resources/objects/next_order_button.gd
@@ -1,10 +1,22 @@
 extends XRToolsInteractableAreaButton
 
+const PRESS_SOUND: AudioStream = preload("res://audio/object_interaction/order_button/order_button_pressed.mp3")
+const RELEASE_SOUND: AudioStream = preload("res://audio/object_interaction/order_button/order_button_released.mp3")
+@onready var audio_player: AudioStreamPlayer3D = $AudioStreamPlayer3D
 
-func _on_button_pressed(button: Variant) -> void:
+func _on_button_pressed(_button: Variant) -> void:
+	audio_player.stream = PRESS_SOUND
+	audio_player.pitch_scale = randf_range(1,1.2)
+	audio_player.play()
 	var new_order = OrderController.create_new_order()
 	if new_order != null:
 		var new_paper = load("res://resources/objects/order_paper.tscn").instantiate()
 		$Order_Paper_Spawner.add_child(new_paper)
 		new_paper.set_order(new_order)
 		new_order.order_paper = new_paper
+
+
+func _on_button_released(_button: Variant) -> void:
+	audio_player.stream = RELEASE_SOUND
+	audio_player.pitch_scale = randf_range(1,1.2)
+	audio_player.play()
