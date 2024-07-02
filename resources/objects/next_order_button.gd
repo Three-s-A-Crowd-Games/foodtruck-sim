748e8828be72e9a7006bbb4b112b718ab04ef201
Thyrexx <96577754+Thyrexx@users.noreply.github.com>
2024-07-02 23:51:22 +0200

    FS-135 order print sound

--- resources/objects/next_order_button.gd
+++ resources/objects/next_order_button.gd
@@ -2,14 +2,16 @@ extends XRToolsInteractableAreaButton
 
 const PRESS_SOUND: AudioStream = preload("res://audio/object_interaction/order_button/order_button_pressed.mp3")
 const RELEASE_SOUND: AudioStream = preload("res://audio/object_interaction/order_button/order_button_released.mp3")
-@onready var audio_player: AudioStreamPlayer3D = $AudioStreamPlayer3D
+@onready var button_player: AudioStreamPlayer3D = $ButtonPlayer
+@onready var print_player: AudioStreamPlayer3D = $PrintPlayer
 
 func _on_button_pressed(_button: Variant) -> void:
-	audio_player.stream = PRESS_SOUND
-	audio_player.pitch_scale = randf_range(1,1.2)
-	audio_player.play()
+	button_player.stream = PRESS_SOUND
+	button_player.pitch_scale = randf_range(1,1.2)
+	button_player.play()
 	var new_order = OrderController.create_new_order()
 	if new_order != null:
+		print_player.play()
 		var new_paper = load("res://resources/objects/order_paper.tscn").instantiate()
 		$Order_Paper_Spawner.add_child(new_paper)
 		new_paper.set_order(new_order)
@@ -17,6 +19,6 @@ func _on_button_pressed(_button: Variant) -> void:
 
 
 func _on_button_released(_button: Variant) -> void:
-	audio_player.stream = RELEASE_SOUND
-	audio_player.pitch_scale = randf_range(1,1.2)
-	audio_player.play()
+	button_player.stream = RELEASE_SOUND
+	button_player.pitch_scale = randf_range(1,1.2)
+	button_player.play()
