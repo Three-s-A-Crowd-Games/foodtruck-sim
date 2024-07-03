de017fb564bb6ea839e220d75a722dac3ad0ad5c
Thyrexx <96577754+Thyrexx@users.noreply.github.com>
2024-07-03 18:53:18 +0200

    FS-135 music and half the imlpementation

--- resources/objects/furniture/radio.gd
+++ resources/objects/furniture/radio.gd
@@ -4,10 +4,16 @@ const SOUND_ON = preload("res://audio/sfx/object_interaction/radio/radio_on.mp3"
 const SOUND_OFF = preload("res://audio/sfx/object_interaction/radio/radio_off.mp3")
 const ROTATION_ON = Vector3(-66.6,0,0)
 const ROTATION_OFF = Vector3(-9.6,0,0)
+#TODO: configure this path
+const RADIO_PLAYLIST_PATH = ""
 var is_on = false
 @onready var sfx_player: AudioStreamPlayer3D = $SFXPlayer
 @onready var music_player: AudioStreamPlayer3D = $MusicPlayer
 @onready var on_switch := $radio/Cube_002
+
+func _ready() -> void:
+	music_player.finished.connect(music_player.play)
+	music_player.stream = load(RADIO_PLAYLIST_PATH)
 
 
 func _on_interactable_area_button_button_pressed(button: Variant) -> void:
@@ -15,17 +21,9 @@ func _on_interactable_area_button_button_pressed(button: Variant) -> void:
 	if is_on:
 		sfx_player.stream = SOUND_ON
 		on_switch.rotation = ROTATION_ON
-		if music_player.stream_paused:
-			music_player.stream_paused = false
-		else:
-			music_player.play()
 		sfx_player.finished.connect(music_player.play, CONNECT_ONE_SHOT)
 	else:
 		sfx_player.stream = SOUND_OFF
 		on_switch.rotation = ROTATION_OFF
-		if music_player.playing: music_player.stream_paused = true
+		if music_player.playing: music_player.stop()
 	sfx_player.play()
-
-
-func _on_interactable_area_button_button_released(button: Variant) -> void:
-	pass # Replace with function body.
