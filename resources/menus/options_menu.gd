dda801852945b916ddb09341e458c947e308e9cf
Thyrexx <96577754+Thyrexx@users.noreply.github.com>
2024-07-03 22:41:22 +0200

    option menu code setup

---
+++ resources/menus/options_menu.gd
@@ -0,0 +1,79 @@
+extends VBoxContainer
+
+enum MusciGenres{
+	JAZZ,
+	LATIN,
+	ROCK_SOFT,
+	ROCK_HARD,
+	UNCLASSIFIED
+}
+
+static var selected_genres: Array
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+	var id_main = AudioServer.get_bus_index("Master")
+	var id_sfx = AudioServer.get_bus_index("SFX")
+	var id_ambience = AudioServer.get_bus_index("Ambience")
+	var id_music = AudioServer.get_bus_index("Music")
+	var id_dialog = AudioServer.get_bus_index("Voicelines")
+	
+#region Volume
+	$"categories/sound-volume/sound-volume/slider/main-volume/hbox/HScrollBar".value_changed.connect(
+		func(x):
+			AudioServer.set_bus_volume_db(id_main, linear_to_db(x))
+	)
+	$"categories/sound-volume/sound-volume/slider/sfx-volume/hbox/HScrollBar".value_changed.connect(
+		func(x):
+			AudioServer.set_bus_volume_db(id_sfx, linear_to_db(x))
+	)
+	$"categories/sound-volume/sound-volume/slider/ambience-volume/hbox/HScrollBar".value_changed.connect(
+		func(x):
+			AudioServer.set_bus_volume_db(id_ambience, linear_to_db(x))
+	)
+	$"categories/sound-volume/sound-volume/slider/music-volume/hbox/HScrollBar".value_changed.connect(
+		func(x):
+			AudioServer.set_bus_volume_db(id_music, linear_to_db(x))
+	)
+	$"categories/sound-volume/sound-volume/slider/dialogue-volume/hbox/HScrollBar".value_changed.connect(
+		func(x):
+			AudioServer.set_bus_volume_db(id_dialog, linear_to_db(x))
+	)
+#endregion
+	
+#region Genres
+	$"categories/music-genre/music-genre/jazz-check".toggled.connect(
+		func(x):
+			if x: selected_genres.append(MusciGenres.JAZZ)
+			else: selected_genres.erase(MusciGenres.JAZZ)
+	)
+	$"categories/music-genre/music-genre/latin-check".toggled.connect(
+		func(x):
+			if x: selected_genres.append(MusciGenres.LATIN)
+			else: selected_genres.erase(MusciGenres.LATIN)
+	)
+	$"categories/music-genre/music-genre/rock-soft-check".toggled.connect(
+		func(x):
+			if x: selected_genres.append(MusciGenres.ROCK_SOFT)
+			else: selected_genres.erase(MusciGenres.ROCK_SOFT)
+	)
+	$"categories/music-genre/music-genre/rock-hard-check".toggled.connect(
+		func(x):
+			if x: selected_genres.append(MusciGenres.ROCK_HARD)
+			else: selected_genres.erase(MusciGenres.ROCK_HARD)
+	)
+	$"categories/music-genre/music-genre/unclassified-check".toggled.connect(
+		func(x):
+			if x: selected_genres.append(MusciGenres.UNCLASSIFIED)
+			else: selected_genres.erase(MusciGenres.UNCLASSIFIED)
+	)
+#endregion
+	
+#region Radio Effect
+	$"categories/music-genre/music-genre/retro-effect-check".toggled.connect(
+		func(x):
+			AudioServer.set_bus_effect_enabled(id_music, 0, x)
+	)
+#endregion
+	
