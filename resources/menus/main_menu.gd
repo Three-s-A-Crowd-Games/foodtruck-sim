675664f4c52c1a86460586d4e8342ad28c74f0f2
leolady02 <130653249+leolady02@users.noreply.github.com>
2024-07-04 12:01:49 +0200

    changed design stuff nad added hover textures for the buttons

--- resources/menus/main_menu.gd
+++ resources/menus/main_menu.gd
@@ -2,8 +2,8 @@ extends MenuBase
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$VBoxContainer/StartButton.pressed.connect(scene_switch_requested.emit.bind(MainMenuStage.SceneType.GAME))
-	$VBoxContainer/TutorialButton.pressed.connect(scene_switch_requested.emit.bind(MainMenuStage.SceneType.TUTORIAL))
-	$VBoxContainer/OptionsButton.pressed.connect(scene_switch_requested.emit.bind(MainMenuStage.SceneType.OPTIONS))
-	$VBoxContainer/AboutButton.pressed.connect(scene_switch_requested.emit.bind(MainMenuStage.SceneType.ABOUT))
+	$HBoxContainer/VBoxContainer/StartButton.pressed.connect(scene_switch_requested.emit.bind(MainMenuStage.SceneType.GAME))
+	$HBoxContainer/VBoxContainer/TutorialButton.pressed.connect(scene_switch_requested.emit.bind(MainMenuStage.SceneType.TUTORIAL))
+	$HBoxContainer/VBoxContainer/OptionsButton.pressed.connect(scene_switch_requested.emit.bind(MainMenuStage.SceneType.OPTIONS))
+	$HBoxContainer/VBoxContainer/AboutButton.pressed.connect(scene_switch_requested.emit.bind(MainMenuStage.SceneType.ABOUT))
 	
